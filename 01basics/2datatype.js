e71179886bb3ee76bfeda85adb63a010d09928f0
MANishpatidarr <146443790+MANishpatidarr@users.noreply.github.com>
2023-10-05 13:48:13 +0000

    commit

---
+++ 01basics/2datatype.js
@@ -0,0 +1,4 @@
+"use strict"; //it treat all js code is a newer version.
+
+alert("hello0")
+alert(3+3)
---
+++ 01basics/variables.js
@@ -0,0 +1,16 @@
+const accountId =56665565   //constant variable can't be change
+let accountEmail = "manupatidar76@gmail.com"
+var accountPasswoard = "12345" // var data type is not used now because it occures issues in Block scope and function scope.
+accountCity = "Indore" // its right but we don't have to use this type. 
+
+///accountId = 22    constant variable can't be change
+accountEmail="djd@cdjnss.com"
+accountPasswoard=32233
+accountCity="ranipura"
+
+
+//console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountPasswoard);
+//console.log(accountCity);
+console.table([accountId,accountEmail,accountPasswoard,accountCity])  //table is another console property which show data in complet table formate.
